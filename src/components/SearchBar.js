231b40ad2cadf81ad9799def54f24be938b785f8
wtwerner <wtwerner@gmail.com>
2021-04-28 13:34:56 -0500

    Set up state and callback functions for search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,11 +2,19 @@ import React from 'react';
 import { StyleSheet, TextInput, View } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
     return (
         <View style={styles.backgroundStyle}>
             <FontAwesome name="search" style={styles.iconStyle} />
-            <TextInput style={styles.inputStyle} placeholder="Search" />
+            <TextInput
+                autoCapitalize="none"
+                autoCorrect={false}
+                style={styles.inputStyle}
+                placeholder="Search"
+                value={term}
+                onChangeText={onTermChange}
+                onEndEditing={onTermSubmit}
+            />
         </View>
     );
 };
