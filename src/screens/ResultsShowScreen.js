8a39c6b27db1b9620e7b77915999649bbe7c8c99
wtwerner <wtwerner@gmail.com>
2021-04-29 10:51:43 -0500

    Create ResultsShowScreen

---
+++ src/screens/ResultsShowScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const ResultsShowScreen = () => {
+    return (
+        <View>
+            <Text>Results Show Screen</Text>
+        </View>
+    );
+};
+
+export default ResultsShowScreen;
+
+const styles = StyleSheet.create({});
